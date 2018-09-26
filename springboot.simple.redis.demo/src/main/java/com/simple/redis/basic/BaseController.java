5be5954f0ca2bd4cebfc7e42d4948f54df444bdb
陈波 <>
2018-09-26 16:05:03 +0800

    init

---
+++ springboot.simple.redis.demo/src/main/java/com/simple/redis/basic/BaseController.java
@@ -0,0 +1,10 @@
+package com.simple.redis.basic;
+
+/**
+ * @author chenbo@guworks.cc
+ * @title xxx
+ * @date 2018年09月25日
+ * @since v1.0.0
+ */
+public class BaseController {
+}
---
+++ springboot.simple.redis.demo/src/main/java/com/simple/redis/mq/RedisMsgSubscriber.java
@@ -0,0 +1,10 @@
+package com.simple.redis.mq;
+
+/**
+ * @author chenbo@guworks.cc
+ * @title xxx
+ * @date 2018年09月25日
+ * @since v1.0.0
+ */
+public class RedisMsgSubscriber {
+}
